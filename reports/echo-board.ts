640de263fc56fa66759541035f7ec441a80ca710
Jonathan Clem <jclem@github.com>
2020-08-25 11:57:09 -0400

    Clean up echo-board types

--- reports/echo-board.ts
+++ reports/echo-board.ts
@@ -1,11 +1,15 @@
 import * as os from 'os'
 import {CrawlingTarget} from '../interfaces'
+import {UserConfig} from '../util/config'
 
-const reportType = 'any'
-export {reportType}
+export const reportType = 'any'
 
-export function getDefaultConfiguration(): any {
-  return <any>{
+export type EchoBoardConfig = {
+  'add-age': string
+}
+
+export function getDefaultConfiguration(): EchoBoardConfig {
+  return {
     'add-age': 'days'
   }
 }
@@ -13,7 +17,7 @@ export function getDefaultConfiguration(): any {
 // processing the data does a js map on each items and adds data that the report rendering (generate) needs
 // we will dump the json data used to generate the reports next to the rendered report
 // e.g. this function should look at the transition times and added wip status of yellow, red etc.
-export function process(config, data: any): any {
+export function process<T>(config: UserConfig, data: T): T {
   // TODO: process and add age in hours
   if (config) {
     return data
@@ -21,7 +25,7 @@ export function process(config, data: any): any {
   return data
 }
 
-export function renderMarkdown(targets: CrawlingTarget[], data: any): string {
+export function renderMarkdown<T>(targets: CrawlingTarget[], data: T): string {
   console.log(`rendering for ${targets.length} targets`)
 
   const lines: string[] = []
